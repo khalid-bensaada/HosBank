12a28cb022a9c75edf1a8d12b167b9727dd1667b
abdessamadelouarrag <abdessamad4804@gmail.com>
2026-09-15 14:06:48 +0100

    ajouter partie config base de donne

--- config/database.js
+++ config/database.js
@@ -1,1 +1,12 @@
-    
+import mysql2 from "mysql2";
+import dotenv from "dotenv";
+
+//config dotenv 
+dotenv.config();
+
+export const pool = mysql2.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    named: process.env.DB_NAME
+}).promise();
