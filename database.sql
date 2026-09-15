cb648e2e54f206cc0438e68bbcd1e1b027ebd6b9
khalid-bensaada <kbensaada953@gmail.com>
2026-09-15 11:24:50 +0100

    ajouter les tables de base de donnnee

--- database.sql
+++ database.sql
@@ -1,4 +1,5 @@
 CREATE DATABASE hosbank;
+
 USE hosbank;
 
 
@@ -45,7 +46,7 @@ CREATE TABLE Bénéficiaire (
 );
 
 
-CREATE TABLE Compte_bancaire (
+CREATE TABLE `Compte bancaire` (
     id INT PRIMARY KEY AUTO_INCREMENT,
     numeroCompte VARCHAR(50) UNIQUE NOT NULL,
     iban VARCHAR(34) UNIQUE NOT NULL,
@@ -58,7 +59,7 @@ CREATE TABLE Compte_bancaire (
 );
 
 
-CREATE TABLE Carte_bancaire (
+CREATE TABLE `Carte bancaire` (
      id INT PRIMARY KEY AUTO_INCREMENT,
      numerodeCart VARCHAR(16) UNIQUE NOT NULL,
      dateExperation DATE NOT NULL,
