b7744ab2961413bb31fd253d2d2d111720071f83
khalid-bensaada <kbensaada953@gmail.com>
2026-09-17 11:51:18 +0100

    cree un nevaille controller dashboard pour client et cree un function getUserInfo pour voir les informations comme nom et prenom d utilisateur

---
+++ controllers/clientcontrollers/dashboardcontroller.js
@@ -0,0 +1,31 @@
+import {connection} from "../config/database.js";
+
+export async function getUserInfo(req , res){
+
+    try{
+
+        const userId = req.user?.id;
+
+        if(!userId) {
+            return res.status(401).json({message: 'The Access is impossible'});
+        }
+
+        const user = await connection('utilisateur')
+            .select('nom','prenom')
+            .where({ id: userId })
+            .first();
+
+        if(!user){
+            return res.status(404).json({ message: 'undefined User'});
+        }
+
+        return res.status(200).json({
+            nom: user.nom,
+            prenom: user.prenom
+        });
+    }
+    catch (error){
+        console.error('Error about get user',error);
+        return res.status(500).json({ message: 'error in server'});
+    }
+};
