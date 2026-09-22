d71771044bd486e21a41378c463f3749997cd79f
khalid-bensaada <kbensaada953@gmail.com>
2026-09-22 14:34:47 +0000

    modifie le code

--- controllers/admincontrollers/dashboardcontroller.js
+++ controllers/admincontrollers/dashboardcontroller.js
@@ -1,11 +1,9 @@
 import dayjs from "dayjs";
-import {connection} from "../config/database.js";
+import { connection } from "../../config/database.js";
 
-export async function renderAdminDashboard(req ,res){
-
-    try{
-
-        if(!req.user){
+export async function renderAdminDashboard(req, res) {
+    try {
+        if (!req.user) {
             return res.redirect('/login');
         }
 
@@ -14,49 +12,53 @@ export async function renderAdminDashboard(req ,res){
             user: req.user
         });
     }
-    catch(error){
+    catch (error) {
         return res.status(500).json({
             success: false,
-            message:  "something wrong here",
+            message: "something wrong here",
             error: error.message
         });
     }
 }
 
-export async function getDashboardStats(req ,res){
-    try{
-
+export async function getDashboardStats(req, res) {
+    try {
         if (!req.user) {
             return res.status(401).json({
                 message: 'can not using this'
             });
         }
 
-        const totalUsers = await User.count();
+        const totalUsersResult = await connection('Utilisateur')
+            .count('id as count')
+            .first();
+        const totalUsers = parseInt(totalUsersResult?.count) || 0;
 
-        const totalAccounts = await Account.count();
+        const totalAccountsResult = await connection('Compte bancaire')
+            .count('id as count')
+            .first();
+        const totalAccounts = parseInt(totalAccountsResult?.count) || 0;
 
-        const totalMoney = await Account.sum('balance') || 0;
+        const totalMoneyResult = await connection('Compte bancaire')
+            .sum('solde as total')
+            .first();
+        const totalMoney = parseFloat(totalMoneyResult?.total) || 0;
 
         const startOfToday = dayjs().startOf('day').toDate();
 
-        const todayVirments = await Transaction.count({
-            where: {
-                createdAt: {
-                    [Op.gte]: startOfToday
-                }
-            }
-        });
+        const todayVirementsResult = await connection('Virement')
+            .where('dateCreation', '>=', startOfToday)
+            .count('id as count')
+            .first();
+        const todayVirements = parseInt(todayVirementsResult?.count) || 0;
 
         const startOfMonth = dayjs().startOf('month').toDate();
 
-        const monthVirments = await Transaction.count({
-            where: {
-                createdAt: {
-                    [Op.gte]: startOfMonth
-                }
-            }
-        });
+        const monthVirementsResult = await connection('Virement')
+            .where('dateCreation', '>=', startOfMonth)
+            .count('id as count')
+            .first();
+        const monthVirements = parseInt(monthVirementsResult?.count) || 0;
 
         return res.status(200).json({
             success: true,
@@ -64,19 +66,16 @@ export async function getDashboardStats(req ,res){
                 totalUsers,
                 totalAccounts,
                 totalMoney,
-                todayVirments,
-                monthVirments
+                todayVirements,
+                monthVirements
             }
         });
 
-
-
-
     }
-    catch(error){
+    catch (error) {
         return res.status(500).json({
             success: false,
-            message:  "something wrong here",
+            message: "something wrong here",
             error: error.message
         });
     }
