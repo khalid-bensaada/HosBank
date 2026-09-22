3677589afb86aaebf65d1a0ef984bddcffe4f3fe
khalid-bensaada <kbensaada953@gmail.com>
2026-09-22 11:38:34 +0000

    ajouter un function getdashboardstats

--- controllers/admincontrollers/dashboardcontroller.js
+++ controllers/admincontrollers/dashboardcontroller.js
@@ -1,4 +1,17 @@
 import dayjs from "dayjs";
 import {connection} from "../config/database.js";
 
+export async function getDashboardStats(req ,res){
+    try{
 
+        if (!req.user) {
+            return res.status(401).json({
+                message: 'can not using this'
+            });
+        }
+
+    }
+    catch(error){
+
+    }
+}
