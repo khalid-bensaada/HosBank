788c3622e9e8b96600e75ac24d852d1932672174
khalid-bensaada <kbensaada953@gmail.com>
2026-09-25 09:33:29 +0000

    cree le view de parte carte et carte details

--- routes/adminroute.js
+++ routes/adminroute.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { authMiddleware } from '../middleware/authMiddleware.js';
 import { isAdmin } from '../middleware/isAdmin.js';
+
 import {
     renderAdminDashboard,
     getDashboardStats,
     getRecentTransactions
 } from '../controllers/admincontrollers/dashboardcontroller.js';
+
 import {
     renderUtilisateursPage,
     renderUtilisateurDetailsPage,
@@ -15,6 +17,7 @@ import {
     updateUtilisateurRole,
     getAllRoles
 } from '../controllers/admincontrollers/utilisateurcontroller.js';
+
 import {
     renderComptesPage,
     getAllComptes,
@@ -23,13 +26,23 @@ import {
     toggleCompteStatus
 } from '../controllers/admincontrollers/comptecontroller.js';
 
+import {
+    renderCartesPage,
+    renderCarteDetailsPage,
+    getAllCartes,
+    getCartesStats,
+    getCarteById,
+    toggleCarteStatus
+} from '../controllers/admincontrollers/cartecontroller.js';
 
 const router = express.Router();
 
+// Dashboard
 router.get('/dashboard', authMiddleware, isAdmin, renderAdminDashboard);
 router.get('/api/dashboard/stats', authMiddleware, isAdmin, getDashboardStats);
 router.get('/api/dashboard/recent-transactions', authMiddleware, isAdmin, getRecentTransactions);
 
+// Utilisateurs
 router.get('/utilisateurs', authMiddleware, isAdmin, renderUtilisateursPage);
 router.get('/utilisateurs/:userId', authMiddleware, isAdmin, renderUtilisateurDetailsPage);
 router.get('/api/utilisateurs', authMiddleware, isAdmin, getAllUtilisateurs);
@@ -38,10 +51,19 @@ router.put('/api/utilisateurs/:userId/toggle-status', authMiddleware, isAdmin, t
 router.put('/api/utilisateurs/:userId/role', authMiddleware, isAdmin, updateUtilisateurRole);
 router.get('/api/roles', authMiddleware, isAdmin, getAllRoles);
 
+// Comptes
 router.get('/comptes', authMiddleware, isAdmin, renderComptesPage);
 router.get('/api/comptes', authMiddleware, isAdmin, getAllComptes);
 router.get('/api/comptes/stats', authMiddleware, isAdmin, getComptesStats);
 router.get('/api/comptes/:compteId', authMiddleware, isAdmin, getCompteById);
 router.put('/api/comptes/:compteId/toggle-status', authMiddleware, isAdmin, toggleCompteStatus);
+
+// Cartes
+router.get('/cartes', authMiddleware, isAdmin, renderCartesPage);
+router.get('/cartes/:carteId', authMiddleware, isAdmin, renderCarteDetailsPage);
+router.get('/api/cartes', authMiddleware, isAdmin, getAllCartes);
+router.get('/api/cartes/stats', authMiddleware, isAdmin, getCartesStats);
+router.get('/api/cartes/:carteId', authMiddleware, isAdmin, getCarteById);
+router.put('/api/cartes/:carteId/toggle-status', authMiddleware, isAdmin, toggleCarteStatus);
 
 export default router;
